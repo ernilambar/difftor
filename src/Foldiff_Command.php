d7b6cf6f9813639bcacd12b2f5da9e9e99f54fdb
Nilambar Sharma <nilambar@outlook.com>
2025-11-24 21:59:44 +0545

    Add files

---
+++ src/Foldiff_Command.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Foldiff_Command
+ *
+ * @package Foldiff_Command
+ */
+
+namespace Nilambar\Foldiff_Command;
+
+/**
+ * Foldiff_Command Class.
+ *
+ * @since 1.0.0
+ */
+class Foldiff_Command {
+
+	public function generate( $args, $assoc_args = [] ) {
+		echo 'yo';
+	}
+}
