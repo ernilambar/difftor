8094a6d5985a7957f9d2b3dd3b66c00309f36c84
Nilambar Sharma <nilambar@outlook.com>
2025-11-28 23:06:36 +0545

    Fix title

--- src/Utils/HTML_Utils.php
+++ src/Utils/HTML_Utils.php
@@ -95,7 +95,7 @@ class HTML_Utils {
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Folder Diff</title>
+	<title>Difftor</title>
 	<style>
 		body {
 			font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
